9b65e445f6c7358d9098bac84a33d1749d1fe337
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-03-22 21:18:13 +0100

    Working on the data model

--- tshirts/models.py
+++ tshirts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Tshirt(models.Model):
+    Sizes = ('XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL')
+
+    brand = models.CharField(max_length=256, verbose_name='Brand name')
+    design = models.CharField(max_length=256, verbose_name='Design name')
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    size = models.CharField(choices=Sizes, max_length=3, default='M')
+    # image =
+    # video =
+
+
+    def __str__(self):
+        return f'{self.design} - {self.brand} - created {self.created}'
+
+
+    class Meta:
+        verbose_name = 'T-shirt'
+        verbose_name_plural = 'T-shirts'
+
+class Story(models.Model):
+    body = models.TextField(max_length=400)
+    stars = models.IntegerField(default=5,
+                                    validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created = models.DateTimeField(auto_now_add=True)
+    tshirt = models.ForeignKey('T-shirt', on_delete=models.CASCADE)
