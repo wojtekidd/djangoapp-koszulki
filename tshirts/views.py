7dc1871582ac0722ce6694423d9b7ab30c3034b8
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-03-23 17:03:10 +0100

    Working on displaying all uploaded t-shirts

--- tshirts/views.py
+++ tshirts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Tshirt
 
 # Create your views here.
+
+
+class HomePageView(ListView):
+    model = Tshirt
+    template_name = 'home.html'
