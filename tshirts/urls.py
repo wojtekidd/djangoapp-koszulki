59bbabca785d72cfe6e9076a7663f4c7f04add7f
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-04-07 15:36:34 +0200

    Working on the views

--- tshirts/urls.py
+++ tshirts/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='index'),
     path('add/', CreateTshirtView.as_view(), name='add_tshirt'),
     path("login/", auth_views.LoginView.as_view(), name='login'),
     path("logout", auth_views.LogoutView.as_view(), name='logout'),
--- tshirts/views.py
+++ tshirts/views.py
@@ -1,13 +1,11 @@
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
-from django.shortcuts import render
 
 from .forms import TshirtForm
 from .models import Tshirt
 
 
 # Create your views here.
-
 
 class HomePageView(ListView):
     model = Tshirt
@@ -21,17 +19,4 @@ class CreateTshirtView(CreateView):
     success_url = reverse_lazy('index')
 
 
-def index(request):
-    """View function for home page of site."""
 
-    # Generate counts of some of the main objects
-    num_tshirts = Tshirt.objects.all().count()
-
-    # TODO Add brands and stories here so that they get reflected on the homepage
-
-    context = {
-        'num_tshirts': num_tshirts,
-    }
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
