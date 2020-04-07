930345fa77885cc03f7954f0747652be44a2c05d
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-04-07 14:52:52 +0200

    Working on the views

--- tshirts/views.py
+++ tshirts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from .forms import TshirtForm
 from .models import Tshirt
@@ -10,12 +11,27 @@ from .models import Tshirt
 
 class HomePageView(ListView):
     model = Tshirt
-    template_name = 'home.html'
+    template_name = 'index.html'
 
 
 class CreateTshirtView(CreateView):
     model = Tshirt
     form_class = TshirtForm
     template_name = 'add_tshirt.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('index')
 
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_tshirts = Tshirt.objects.all().count()
+
+    # TODO Add brands and stories here so that they get reflected on the homepage
+
+    context = {
+        'num_tshirts': num_tshirts,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
