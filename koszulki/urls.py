bae16c7286dd7e36dab4837df88ed453a983fdd8
Wojtek Cichon <wojtek.cichon@protonmail.com>
2020-04-26 15:00:48 +0200

    Moved auth_views to koszulki.urls

--- koszulki/urls.py
+++ koszulki/urls.py
@@ -18,10 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tshirts/', include('tshirts.urls')),
     path('register/', user_views.register, name='register' ),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("login/", auth_views.LoginView.as_view(), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
