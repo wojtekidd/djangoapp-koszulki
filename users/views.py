e9f6c272de2bcbd7ee18466e3df0a3da26dc6259
karolina <wojciechowska.carolina@gmail.com>
2020-04-30 08:32:45 +0200

    @login_required added to profile.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -15,6 +16,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
