343f3e017a0d8ce389ff4648867fafd01841f237
Damian Szymanski <szymanski.damian@op.pl>
2020-05-09 11:41:19 +0200

    Email form created

--- tshirts/views.py
+++ tshirts/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
-from django.shortcuts import render
-
-from .forms import TshirtForm
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from .forms import *
 from .models import Tshirt, Story
+
 
 
 # Create your views here.
@@ -63,3 +65,39 @@ class StoryDetail(DetailView):
 
     def get_queryset(self):
         return Story.objects
+
+
+"""Contact form-view"""
+def Contact(request):
+    Contact_Form = ContactForm
+    if request.method == 'POST':
+        form = Contact_Form(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name')
+            contact_email = request.POST.get('contact_email')
+            contact_content = request.POST.get('content')
+
+        template = get_template('contact_form.txt')
+
+        content = {
+            'contact_name': contact_name,
+            'contact_email': contact_email,
+            'contact_content': contact_content
+        }
+
+        content = template.render(content)
+
+        email = EmailMessage(
+            "New contact form",
+            content,
+            "KoszulkiApp" + '',
+            ['koszulkistore@gmail.com'],
+            headers= {'Reply to': contact_email}
+        )
+
+        email.send()
+
+        return redirect('index')
+
+    return render(request, 'contact.html', {'form': Contact_Form})
