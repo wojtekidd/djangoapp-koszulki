aa10d99da3e7e3e43b7613c6acbf2dc0740823bc
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-05-11 20:14:21 +0200

    Added referenece to default.png

--- users/models.py
+++ users/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics', null=True)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
