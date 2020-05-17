7145c6dcdb2e0e2193a75b50eccafa4a97e584b4
Wojtek Cichon <wojciech.cichon@gmail.com>
2020-05-17 11:12:06 +0200

    added migrations

--- koszulki/settings.py
+++ koszulki/settings.py
@@ -161,12 +161,12 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # example cloudcube url: https://cloud-cube.s3.amazonaws.com/bucketname
 cloudcube_url = os.environ.get('CLOUDCUBE_URL')
-cloudcube_bucket = os.path.basename(cloudcube_url)   # "bucketname"
-cloudcube_base_url = os.path.dirname(cloudcube_url)  # "https://cloud-cube.s3.amazonaws.com/"
+cloudcube_bucket = os.path.basename('l7l6vu9pmol3')   # "bucketname"
+cloudcube_base_url = os.path.dirname('https://cloud-cube.s3.amazonaws.com/l7l6vu9pmol3')  # "https://cloud-cube.s3.amazonaws.com/"
 
 AWS_S3_ENDPOINT_URL = cloudcube_base_url
 AWS_ACCESS_KEY_ID = os.environ.get('CLOUDCUBE_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('CLOUDCUBE_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = cloudcube_bucket
-AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', 'public-read)
+AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', 'public-read')
 AWS_QUERYSTRING_AUTH = False
